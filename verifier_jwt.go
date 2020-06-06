a9f618563ed380b9e1cd3491d51585b0f716a277
Sureshkumar <suresh241098@gmail.com>
2020-06-06 16:49:46 +0530

    all working- get_public function working

--- verifier_jwt.go
+++ verifier_jwt.go
@@ -18,6 +18,12 @@ import (
 var keys map[string]rsa.PublicKey
 
 func main() {
+	new_keys := get_publickeys()
+	fmt.Println("The new keys : ", new_keys)
+}
+
+
+func get_publickeys() map[string]rsa.PublicKey {
 	jwk_get_resp,_ := http.Get("https://www.googleapis.com/oauth2/v3/certs")
 	jwk_cont,_ := ioutil.ReadAll(jwk_get_resp.Body)
 
@@ -33,35 +39,52 @@ func main() {
 	n := regex_n.FindAllSubmatch(jwk_cont, -1)
 	e := regex_e.FindAllSubmatch(jwk_cont, -1)
 
-	fmt.Printf("\nThe KID-1 : %s\n", kid[0][1])
-	fmt.Printf("\nThe N-1 : %s\n", n[0][1])
-	fmt.Printf("\nThe E-1 : %s\n\n\n", e[0][1])
+	// fmt.Printf("\nThe KID-1 : %s\n", kid[0][1])
+	// fmt.Printf("\nThe N-1 : %s\n", n[0][1])
+	// fmt.Printf("\nThe E-1 : %s\n\n\n", e[0][1])
 
-	fmt.Printf("\nThe KID-2 : %s\n", kid[1][1])
-	fmt.Printf("\nThe N-2 : %s\n", n[1][1])
-	fmt.Printf("\nThe E-2 : %s\n", e[1][1])
+	// fmt.Printf("\nThe KID-2 : %s\n", kid[1][1])
+	// fmt.Printf("\nThe N-2 : %s\n", n[1][1])
+	// fmt.Printf("\nThe E-2 : %s\n", e[1][1])
 
 	n1 := byteToInt(urlsafeB64decode(string(n[0][1])))
-	fmt.Printf("\nThe N-1 : %s\n", n1)
+	//fmt.Printf("\nThe N-1 : %s\n", n1)
 	n2 := byteToInt(urlsafeB64decode(string(n[1][1])))
-	fmt.Printf("\nThe N-2 : %s\n", n2)
+	//fmt.Printf("\nThe N-2 : %s\n", n2)
 
 	e1 := btrToInt(byteToBtr(urlsafeB64decode(string(e[0][1]))))
-	fmt.Printf("\nThe E-1 : %d\n", e1)
+	//fmt.Printf("\nThe E-1 : %d\n", e1)
 	e2 := btrToInt(byteToBtr(urlsafeB64decode(string(e[1][1]))))
-	fmt.Printf("\nThe E-2 : %d\n", e2)
+	//fmt.Printf("\nThe E-2 : %d\n", e2)
 
 	public_key1 := rsa.PublicKey{N:n1, E:e1}
-	fmt.Println("The PublicKey-1 : ", public_key1)
+	//fmt.Println("The PublicKey-1 : ", public_key1)
 	public_key2 := rsa.PublicKey{N:n2, E:e2}
-	fmt.Println("The PublicKey-2 : ", public_key2)
+	//fmt.Println("The PublicKey-2 : ", public_key2)
 
 	keys := map[string]rsa.PublicKey{
 		string(kid[0][1]):public_key1,
 		string(kid[1][1]):public_key2,
 	}
-	fmt.Println("The Keys : ", keys)
+	//fmt.Println("The Keys : ", keys)
+
+	return keys
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //////////////////////// Helper Functions ////////////////////////////////////

d1d29fa5633d39ea2593657c19c375c4d0a01575
Sureshkumar <suresh241098@gmail.com>
2020-06-06 16:41:01 +0530

    all working

--- sign_verify.go
+++ sign_verify.go
@@ -11,37 +11,14 @@ import (
 	"crypto/rsa"
 	"crypto/sha256"
 	"strings"
+	"regexp"
 )
 
 func main() {
-	b64_n := "wl6TaY_3dsuLczYH_hioeQ5JjcLKLGYb--WImN9_IKMkOj49dgs25wkjsdI9XGJYhhPJLlvfjIfXH49ZGA_XKLx7fggNaBRZcj1y-I3_77tVa9N7An5JLq3HT9XVt0PNTq0mtX009z1Hva4IWZ5IhENx2rWlZOfFAXiMUqhnDc8VY3lG7vr8_VG3cw3XRKvlZQKbb6p2YIMFsUwaDGL2tVF4SkxpxIazUYfOY5lijyVugNTslOBhlEMq_43MZlkznSrbFx8ToQ2bQX4Shj-r9pLyofbo6A7K9mgWnQXGY5rQVLPYYRzUg0ThWDzwHdgxYC5MNxKyQH4RC2LPv3U0LQ"
-
-	b64_e := "AQAB"
-
-	//n,_ := base64.URLEncoding.DecodeString(b64_n)
-	//fmt.Println("N: ", n)
-
-	n_int := byteToInt(urlsafeB64decode(b64_n))
-	//n_int.SetBytes(n)
-	fmt.Println("N_Int : ", n_int)
-
-	e,_ := base64.URLEncoding.DecodeString(b64_e)
-	fmt.Println("E: ", e)
-
-	//e_reader := bytes.NewReader(e)
-	//fmt.Println("E_reader : ", e_reader)
-	//var e_io_reader io.Reader = e_reader
-	//var e_int uint6r
-	//binary.Read(e_io_reader, binary.BigEndian, &e_int)
-	//fmt.Println("E_int : ", int(e_int))
-
-	e_btr := byteToBtr(e)
-	fmt.Println("E_btr: ", e_btr)
-	e_int := btrToInt(e_btr)
-	fmt.Println("E_int: ", e_int)
-
-	public_key := rsa.PublicKey{N: n_int,E: e_int}
-	fmt.Printf("Public key: %q, %T", public_key,public_key)
+	n_vals := map[string]string{
+		"fb8ca5b7d8d9a5c6c6788071e866c6c40f3fc1f9":"zK8PHf_6V3G5rU-viUOL1HvAYn7q--dxMoUkt7x1rSWX6fimla-lpoYAKhFTLUELkRKy_6UDzfybz0P9eItqS2UxVWYpKYmKTQ08HgUBUde4GtO_B0SkSk8iLtGh653UBBjgXmfzdfQEz_DsaWn7BMtuAhY9hpMtJye8LQlwaS8ibQrsC0j0GZM5KXRITHwfx06_T1qqC_MOZRA6iJs-J2HNlgeyFuoQVBTY6pRqGXa-qaVsSG3iU-vqNIciFquIq-xydwxLqZNksRRer5VAsSHf0eD3g2DX-cf6paSy1aM40svO9EfSvG_07MuHafEE44RFvSZZ4ubEN9U7ALSjdw",
+		"492710a7fcdb153960ce01f760520a32c8455dff":"wl6TaY_3dsuLczYH_hioeQ5JjcLKLGYb--WImN9_IKMkOj49dgs25wkjsdI9XGJYhhPJLlvfjIfXH49ZGA_XKLx7fggNaBRZcj1y-I3_77tVa9N7An5JLq3HT9XVt0PNTq0mtX009z1Hva4IWZ5IhENx2rWlZOfFAXiMUqhnDc8VY3lG7vr8_VG3cw3XRKvlZQKbb6p2YIMFsUwaDGL2tVF4SkxpxIazUYfOY5lijyVugNTslOBhlEMq_43MZlkznSrbFx8ToQ2bQX4Shj-r9pLyofbo6A7K9mgWnQXGY5rQVLPYYRzUg0ThWDzwHdgxYC5MNxKyQH4RC2LPv3U0LQ",
+	}
 
 	fmt.Printf("Enter token : ")
 	var token_raw string
@@ -55,11 +32,34 @@ func main() {
 	fmt.Printf("body : %s\n", body)
 	fmt.Printf("\n\nSignature : %s\n\n", signature)
 	
-	//msgToSign := token_split[0] + "." + token_split[1]
-	//fmt.Printf("Message To sign : %s\n", msgToSign)
+	re_kid := regexp.MustCompile(`.kid...([\d\w]+).`)
+	kid := re_kid.FindStringSubmatch(string(header))
+	fmt.Println("KID : ", kid) 
+	fmt.Println("N for KID : ", n_vals[kid[1]])
 
-//	msg_sum := sha256.New()
-//	msg_sum.Write([]byte(msgToSign))
+	//b64_n := "wl6TaY_3dsuLczYH_hioeQ5JjcLKLGYb--WImN9_IKMkOj49dgs25wkjsdI9XGJYhhPJLlvfjIfXH49ZGA_XKLx7fggNaBRZcj1y-I3_77tVa9N7An5JLq3HT9XVt0PNTq0mtX009z1Hva4IWZ5IhENx2rWlZOfFAXiMUqhnDc8VY3lG7vr8_VG3cw3XRKvlZQKbb6p2YIMFsUwaDGL2tVF4SkxpxIazUYfOY5lijyVugNTslOBhlEMq_43MZlkznSrbFx8ToQ2bQX4Shj-r9pLyofbo6A7K9mgWnQXGY5rQVLPYYRzUg0ThWDzwHdgxYC5MNxKyQH4RC2LPv3U0LQ"
+	b64_n := n_vals[kid[1]]
+	
+	b64_e := "AQAB"
+
+
+	fmt.Println("MAP : ", n_vals)
+
+
+	n_int := byteToInt(urlsafeB64decode(b64_n))
+	fmt.Println("N_Int : ", n_int)
+
+	e,_ := base64.URLEncoding.DecodeString(b64_e)
+	fmt.Println("E: ", e)
+	e_btr := byteToBtr(e)
+	fmt.Println("E_btr: ", e_btr)
+	e_int := btrToInt(e_btr)
+	fmt.Println("E_int: ", e_int)
+
+	public_key := rsa.PublicKey{N: n_int,E: e_int}
+	fmt.Printf("Public key: %q, %T", public_key,public_key)
+
+
 	msg_byte := calcSum(token_split[0]+"."+token_split[1])
 	fmt.Println("Message Sum : \n", msg_byte)
 
---
+++ verifier_jwt.go
@@ -0,0 +1,91 @@
+package main 
+
+import (
+	"fmt"
+	"net/http"
+	"io/ioutil"
+	"regexp"
+	"bytes"
+	"math/big"
+	"encoding/base64"
+	"encoding/binary"
+	"crypto/sha256"
+	"crypto/rsa"
+	"io"
+	"strings"
+)
+
+
+
+func main() {
+	jwk_get_resp,_ := http.Get("https://www.googleapis.com/oauth2/v3/certs")
+	jwk_cont,_ := ioutil.ReadAll(jwk_get_resp.Body)
+
+	// cont_string := string(content)
+	// resp.Body.Close()
+	// fmt.Println("The body : ", cont_string)	
+
+	regex_kid := regexp.MustCompile(`"kid": "(\S+)"`)
+	regex_n := regexp.MustCompile(`"n": "(\S+)"`)
+	regex_e := regexp.MustCompile(`"e": "(\S+)"`)
+
+	kid := regex_kid.FindAllSubmatch(jwk_cont, -1)
+	n := regex_n.FindAllSubmatch(jwk_cont, -1)
+	e := regex_e.FindAllSubmatch(jwk_cont, -1)
+
+	fmt.Printf("\nThe KID-1 : %s\n", kid[0][1])
+	fmt.Printf("\nThe N-1 : %s\n", n[0][1])
+	fmt.Printf("\nThe E-1 : %s\n\n\n", e[0][1])
+
+	fmt.Printf("\nThe KID-2 : %s\n", kid[1][1])
+	fmt.Printf("\nThe N-2 : %s\n", n[1][1])
+	fmt.Printf("\nThe E-2 : %s\n", e[1][1])
+
+	n1 := byteToInt(urlsafeB64decode(string(n[0][1])))
+	fmt.Printf("\nThe N-1 : %s\n", n1)
+	n2 := byteToInt(urlsafeB64decode(string(n[1][1])))
+	fmt.Printf("\nThe N-2 : %s\n", n2)
+
+	e1 := btrToInt(byteToBtr(urlsafeB64decode(string(e[0][1]))))
+	fmt.Printf("\nThe E-1 : %d\n", e1)
+	e2 := btrToInt(byteToBtr(urlsafeB64decode(string(e[1][1]))))
+	fmt.Printf("\nThe E-2 : %d\n", e2)
+
+	public_key1 := rsa.PublicKey{N:n1, E:e1}
+	fmt.Println("The PublicKey-1 : ", public_key1)
+	public_key2 := rsa.PublicKey{N:n2, E:e2}
+	fmt.Println("The PublicKey-2 : ", public_key2)
+}
+//////////////////////// Helper Functions ////////////////////////////////////
+func byteToBtr(bt0 []byte) *bytes.Reader {
+	var bt1 []byte
+	if len(bt0) < 8 {
+		bt1 = make([]byte, 8-len(bt0), 8)
+		bt1 = append(bt1, bt0...)
+	} else {
+		bt1 = bt0
+	}
+	return bytes.NewReader(bt1)
+}
+func btrToInt(a io.Reader) int {
+	var e uint64
+	binary.Read(a, binary.BigEndian, &e)
+	return int(e)
+}
+func urlsafeB64decode(str string) []byte {
+	if m := len(str) % 4; m != 0 {
+		str += strings.Repeat("=", 4-m)
+	}
+	bt, _ := base64.URLEncoding.DecodeString(str)
+	return bt
+}
+func calcSum(str string) []byte {
+	a := sha256.New()
+	a.Write([]byte(str))
+	return a.Sum(nil)
+}
+func byteToInt(bt []byte) *big.Int {
+	a := big.NewInt(0)
+	a.SetBytes(bt)
+	return a
+}

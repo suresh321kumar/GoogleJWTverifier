e188faade456a5ed9d8e810b4790e27e53a21094
Sureshkumar <suresh241098@gmail.com>
2020-06-07 09:03:59 +0530

    all working- all working, including http, cleaned the main code

--- verifier_jwt_debug.go
+++ verifier_jwt_debug.go
@@ -24,43 +24,95 @@ var current_keys map[string]rsa.PublicKey
 
 func main() {
 	current_keys = get_publickeys()
+
 	http.HandleFunc("/", http_handler)
 	http.ListenAndServe(":8090", nil)
+
+	// fmt.Println("The new keys : ", new_keys)
+
+	// fmt.Printf("Enter token : ")
+	// var token_raw string
+	// fmt.Scanln(&token_raw)
+
+	// header, body, signature, msg_hash, req_kid, email, exptime := parseJWT(token_raw)
+	// fmt.Printf("header : %s\n", header)
+	// fmt.Printf("body : %s\n", body)
+	// fmt.Printf("sig : %s\n", signature)
+	// fmt.Println("msg_hash : ", msg_hash)
+	// fmt.Println("req_kid : ", req_kid)
+	// fmt.Println("Email : ", email)
+	// fmt.Println("exptime : ", exptime)
+
+	// req_pubkey := check_publickeys(current_keys, req_kid)	
+	// fmt.Println("The PublicKey : ", req_pubkey)
+
+	// time_now := time.Now().Unix()
+	// time_expiry,_ := strconv.ParseInt(exptime, 10, 64)
+
+	// if time_now < time_expiry {
+	// 	fmt.Println("Not expired")
+	// 	err := rsa.VerifyPKCS1v15(&req_pubkey, crypto.SHA256, msg_hash, signature)
+	// 	if err != nil {
+	// 		fmt.Println("Error : ", err)
+	// 	} else {
+	// 		fmt.Println("Verified !!!!")
+	// 	}
+
+	// } else {
+	// 	fmt.Println("Error : expired token")
+	// }
+
+
+
 }
 
 func http_handler(w http.ResponseWriter, r *http.Request) {
 	token_raw := r.URL.Query()["token"]
-	
-	signature, msg_hash, req_kid, email, exptime := parseJWT(token_raw[0])
-	req_pubkey := check_publickeys(current_keys, req_kid)	
+	fmt.Printf("Token : %s, %T", token_raw, token_raw)
+	header, body, signature, msg_hash, req_kid, email, exptime := parseJWT(token_raw[0])
+	fmt.Printf("header : %s\n", header)
+	fmt.Printf("body : %s\n", body)
+	fmt.Printf("sig : %s\n", signature)
+	fmt.Println("msg_hash : ", msg_hash)
+	fmt.Println("req_kid : ", req_kid)
+	fmt.Println("Email : ", email)
+	fmt.Println("exptime : ", exptime)
 
+	req_pubkey := check_publickeys(current_keys, req_kid)	
+	fmt.Println("The PublicKey : ", req_pubkey)
 
 	time_now := time.Now().Unix()
 	time_expiry,_ := strconv.ParseInt(exptime, 10, 64)
 
 	if time_now < time_expiry {
+		fmt.Println("Not expired")
 		err := rsa.VerifyPKCS1v15(&req_pubkey, crypto.SHA256, msg_hash, signature)
 		if err != nil {
-			w.WriteHeader(400)
-			fmt.Fprintf(w, "Error : Invalid Token")
+			fmt.Println("Error : ", err)
 		} else {
-			fmt.Fprintf(w,email)
+			fmt.Println("Verified !!!!")
 		}
+
 	} else {
-		w.WriteHeader(400)
-		fmt.Fprintf(w, "Error : expired token")
+		fmt.Println("Error : expired token")
 	}
 
 }
 
+
+
+
+
+
 func check_publickeys(keys map[string]rsa.PublicKey, req_kid string) rsa.PublicKey{
 	for k,v := range keys {
+		//fmt.Println("KID : ",k,"=>","PubKey : ",v)
 		if req_kid == k {
 			req_pubkey = v
 			return req_pubkey
 		} 
 	}
-
+	
 	// get new keys and check again 
 	new_keys := get_publickeys()
 	for k,v := range new_keys {
@@ -72,15 +124,19 @@ func check_publickeys(keys map[string]rsa.PublicKey, req_kid string) rsa.PublicK
 	return req_pubkey
 }
 
-func parseJWT(token_raw string) ([]byte, []byte, string, string, string){
+func parseJWT(token_raw string) ([]byte, []byte, []byte, []byte, string, string, string){
 
 	token_split := strings.Split(token_raw, ".")
 
 	var header = urlsafeB64decode(token_split[0])
 	var payload = urlsafeB64decode(token_split[1])
 	var signature = urlsafeB64decode(token_split[2])
+	//fmt.Printf("header : %s\n", header)
+	//fmt.Printf("body : %s\n", payload)
+	//fmt.Printf("\n\nSignature : %s\n\n", signature)
 
 	msg_byte := calcSum(token_split[0]+"."+token_split[1])
+	//fmt.Println("Message Sum : \n", msg_byte)
 
 	regex_kid := regexp.MustCompile(`.kid...([\w]+).`)
 	regex_email := regexp.MustCompile(`\w+@\w+.\w+`)
@@ -88,18 +144,23 @@ func parseJWT(token_raw string) ([]byte, []byte, string, string, string){
 
 	email := regex_email.FindString(string(payload))
 	exptime := regex_exptime.FindStringSubmatch(string(payload))
-
 	header_kid := regex_kid.FindStringSubmatch(string(header))	
+	//fmt.Println("The Header kid : ", header_kid[1])
 	req_kid := header_kid[1]
+	//fmt.Println("The Header kid : ", req_kid)
 
-	return signature, msg_byte, req_kid, email, exptime[1]
-
+	//fmt.Printf("Types : %T, %T, %T, %T, %T\n\n", header, payload, signature, msg_byte, req_kid)
+	return header, payload, signature, msg_byte, req_kid, email, exptime[1]
 }
 
 
 func get_publickeys() map[string]rsa.PublicKey {
 	jwk_get_resp,_ := http.Get("https://www.googleapis.com/oauth2/v3/certs")
 	jwk_cont,_ := ioutil.ReadAll(jwk_get_resp.Body)
+
+	// cont_string := string(content)
+	// resp.Body.Close()
+	// fmt.Println("The body : ", cont_string)	
 
 	regex_kid := regexp.MustCompile(`"kid": "(\S+)"`)
 	regex_n := regexp.MustCompile(`"n": "(\S+)"`)
@@ -109,18 +170,35 @@ func get_publickeys() map[string]rsa.PublicKey {
 	n := regex_n.FindAllSubmatch(jwk_cont, -1)
 	e := regex_e.FindAllSubmatch(jwk_cont, -1)
 
+	// fmt.Printf("\nThe KID-1 : %s\n", kid[0][1])
+	// fmt.Printf("\nThe N-1 : %s\n", n[0][1])
+	// fmt.Printf("\nThe E-1 : %s\n\n\n", e[0][1])
+
+	// fmt.Printf("\nThe KID-2 : %s\n", kid[1][1])
+	// fmt.Printf("\nThe N-2 : %s\n", n[1][1])
+	// fmt.Printf("\nThe E-2 : %s\n", e[1][1])
+
 	n1 := byteToInt(urlsafeB64decode(string(n[0][1])))
+	//fmt.Printf("\nThe N-1 : %s\n", n1)
 	n2 := byteToInt(urlsafeB64decode(string(n[1][1])))
+	//fmt.Printf("\nThe N-2 : %s\n", n2)
+
 	e1 := btrToInt(byteToBtr(urlsafeB64decode(string(e[0][1]))))
+	//fmt.Printf("\nThe E-1 : %d\n", e1)
 	e2 := btrToInt(byteToBtr(urlsafeB64decode(string(e[1][1]))))
+	//fmt.Printf("\nThe E-2 : %d\n", e2)
 
 	public_key1 := rsa.PublicKey{N:n1, E:e1}
+	//fmt.Println("The PublicKey-1 : ", public_key1)
 	public_key2 := rsa.PublicKey{N:n2, E:e2}
+	//fmt.Println("The PublicKey-2 : ", public_key2)
 
 	keys := map[string]rsa.PublicKey{
 		string(kid[0][1]):public_key1,
 		string(kid[1][1]):public_key2,
 	}
+	//fmt.Println("The Keys : ", keys)
+
 	return keys
 }
 
